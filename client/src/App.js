9557d0fb18b514783c2e311cbf829727dd558cd5
Harry <harryle.au@gmail.com>
2018-06-24 02:51:11 +1000

    add login and register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route path="/" component={Landing} exact />
+          <div className="container">
+            <Route path="/register" component={Register} exact />
+            <Route path="/login" component={Login} exact />
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
